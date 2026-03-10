b9afa0c19ada3f45e40d618c59eb0276425eff8c
Israel Gonzales <gonzalesisraeljoseph@gmail.com>
2026-03-10 11:34:38 +0800

    add files to git

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Learn react with chaicode.com</h1>
+      <p>This is a sub heading.</p>
+    </>
+  );
+}
+
+export default App
+ 
