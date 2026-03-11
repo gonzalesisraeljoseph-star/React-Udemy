c15e76a50e1e73d484cb9893e19b1f92921c41da
Israel Gonzales <gonzalesisraeljoseph@gmail.com>
2026-03-11 18:24:37 +0800

    creating a card
    card w/ img and text

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import './App.css'
 
 function App() {
@@ -6,10 +5,22 @@ function App() {
 
   return (
     <>
-     <h1 className='text-sky-600 dark:text-sky-400 border-4 p-4'>I try to replicate and learn my online course</h1>
-     <p className='m-4 text-xl'> This is my first time creating in React</p>
-     <div>
-      <h1 className='text-rose-400 border-5 m-4 p-4'>This is fun to learn and master React</h1>
+     <h1 className='text-sky-600 dark:text-sky-400 border-4 p-4 rounded-xl'>I try to replicate and learn my online course</h1>
+     <div className='max-w-sm bg-white border border-gray-200 rounded-xl mt-8 shadow overflow-hidden transition shadow'>
+      <img className='w-full h-50 object-cover' src="https://whszephyr.com/wp-content/uploads/2021/03/Capture7836548-e1614624406630.png" alt="sample image" />
+      <div className='p-4'>
+        <h2 className='text-gray-800'>Card Title</h2>
+        <p className='mt-2 text-gray-600 text-sm'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim architecto rem, voluptatum molestias necessitatibus aut.</p>
+        <button className='mt-4 px-4 bg-blue-600 rounded-lg text-white py-2 hover:bg-blue-700'>Buy Now</button>
+      </div>
+     </div>
+      <div className='max-w-sm bg-white border border-gray-200 rounded-xl mt-8 shadow overflow-hidden transition shadow'>
+      <img className='w-full h-50 object-cover' src="https://whszephyr.com/wp-content/uploads/2021/03/Capture7836548-e1614624406630.png" alt="sample image" />
+      <div className='p-4'>
+        <h2 className='text-gray-800'>Card Title</h2>
+        <p className='mt-2 text-gray-600 text-sm'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim architecto rem, voluptatum molestias necessitatibus aut.</p>
+        <button className='mt-4 px-4 bg-blue-600 rounded-lg text-white py-2 hover:bg-blue-700'>Buy Now</button>
+      </div>
      </div>
     </>
   );
