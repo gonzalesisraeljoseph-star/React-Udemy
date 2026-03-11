4dc3bef5965a36646d33d20d52e77c6fa7854dff
Israel Gonzales <gonzalesisraeljoseph@gmail.com>
2026-03-11 15:03:21 +0800

    my first creation
    recaping my learning yesterday

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+
 import './App.css'
 
 function App() {
+
+
   return (
     <>
-      <h1>Learn react with chaicode.com</h1>
-      <p>I would love to change this.</p>
+     <h1 className='text-sky-600 dark:text-sky-400 border-4 p-4'>I try to replicate and learn my online course</h1>
+     <p className='m-4 text-xl'> This is my first time creating in React</p>
+     <div>
+      <h1 className='text-rose-400 border-5 m-4 p-4'>This is fun to learn and master React</h1>
+     </div>
     </>
   );
 }
 
 export default App
- 
